ae82edadec550630ba4186d90c9950ab15dd3ee3
Marianna <marianna.albrici@gmail.com>
2023-05-03 16:24:45 +0200

    Objects fixed (no print)

--- src/main/Carrello.java
+++ src/main/Carrello.java
@@ -1,15 +1,21 @@
 package main;
 
+import java.util.Arrays;
+
 public class Carrello {
 	
-	String[] elencoArticoli;
+	Object[] elencoArticoli;
 	double totaleCarrello;
 	String cliente;
 	
-	public Carrello(String cliente, String[] elencoArticoli, double totaleCarrello) {
+	public Carrello(String cliente, Object[] elencoArticoli, double totaleCarrello) {
 		this.cliente = cliente;
 		this.elencoArticoli = elencoArticoli;
 		this.totaleCarrello = totaleCarrello;
+	}
+	
+	public static String toString(Carrello carrello) {
+		return carrello.cliente + " " + Arrays.toString(carrello.elencoArticoli) + " " + "$"  +carrello.totaleCarrello;
 	}
 	
 }
--- src/main/Sim.java
+++ src/main/Sim.java
@@ -6,25 +6,25 @@ public class Sim {
 	
 	long number;
 	double credit;
-	String[] calls;
 	double duration;
 	
 	public Sim(long number) {
 		this.number = number;
 		this.credit = 00.00;
-		this.calls = new String[5];
 	}
 	
-	public String[] Calls(double duration, long number) {
-		this.duration = duration;
-		this.number = number;
-		return calls;
-	}
+	Calls call = new Calls(number, 987654, 1.30);
+	Calls call2 = new Calls(number, 198765, 5.37);
+	Calls call3 = new Calls(number, 234567, 4.01);
+	Calls call4 = new Calls(number, 456789, 0.48);
+	Calls call5 = new Calls(number, 675432, 2.26);
+	
+	Object[] previousCalls = {call, call2, call3, call4, call5};
 	
 	public void printPhoneData(Sim sim) {
 		System.out.println("Phone number: " + sim.number);
 		System.out.println("Phone credit: " + sim.credit);
-		System.out.println("Last 5 calls: " + Arrays.toString(sim.calls));
+		System.out.println("Last 5 calls: " + Arrays.toString(previousCalls));
 	}
 
 }
